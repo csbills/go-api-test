8a1ef688b25466ae5ff5412b28d90b393c2573f0
Caio Henrique <caio.henrique1000@hotmail.com>
2026-05-20 16:27:30 -0300

    first commit

---
+++ cmd/api/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		response := Response{
+			Message: "Hello World",
+		}
+
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	addr := ":8080"
+
+	log.Printf("API server running on http://localhost%s", addr)
+
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/csbills/go-api-test
+
+go 1.26.3
